73a2ef5951a54e508898ff26a935a91a820c2ab9
Louis Beaumont <louis.beaumont@gmail.com>
2023-12-10 12:08:43 -0800

    feat: code-interpreter v0

--- assistants-code-interpreter/src/main.rs
+++ assistants-code-interpreter/src/main.rs
@@ -12,12 +12,12 @@ use assistants_core::function_calling::FunctionCallInput;
 use assistants_core::function_calling::ModelConfig;
 use assistants_core::function_calling::Parameter;
 use assistants_core::function_calling::Property;
-use assistants_extra::llm::llm;
+use bollard::container::LogOutput;
 use bollard::container::{
-    Config, CreateContainerOptions, LogsOptions, RemoveContainerOptions, StartContainerOptions,
-    StopContainerOptions,
+    Config, CreateContainerOptions, RemoveContainerOptions, StartContainerOptions,
 };
-use bollard::errors::Error;
+use bollard::exec::CreateExecOptions;
+use bollard::exec::StartExecResults;
 use bollard::models::HostConfig;
 use bollard::Docker;
 use futures::stream::StreamExt;
@@ -32,23 +32,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     io::stdout().flush()?;
     let mut user_input = String::new();
     io::stdin().read_line(&mut user_input)?;
+    let result = interpreter(user_input).await?;
+    println!("Result: {}", result);
+    Ok(())
+}
 
+async fn interpreter(user_input: String) -> Result<String, Box<dyn std::error::Error>> {
     println!("Generating Python code...");
 
     let build_prompt = |user_input: &str| {
         format!("
+You are an Assistant that generate Python code from user input to do complex computations. We execute the code you will generate and return the result to the user.
 Given this user input: {}, generate Python code that we will execute and return the result to the user.
 
 Rules:
 - You can use these libraries: pandas numpy matplotlib scipy
 - Only return Python code. If you return anything else it will trigger a chain reaction that will destroy the universe. All humans will die and you will disappear from existence.
+- Make sure to use the right numbers e.g. with the user ask for the square root of 2, you should return math.sqrt(2) and not math.sqrt(pd.DataFrame({{'A': [1, 2, 3], 'B': [4, 5, 6]}})).
+- Do not use any library if it's simple math (e.g. no need to use pandas to compute the square root of 2)
 
 A few examples:
 
 The user input is: compute the square root of pi
 The Python code is:
 import math
-print(math.sqrt(math.pi))
+print(\"The square root of pi is: \" + str(math.sqrt(math.pi)))
 
 The user input is: raising $27M at a $300M valuation how much dilution will the founders face if they raise a $58M Series A at a $2B valuation?
 The Python code is:
@@ -62,7 +70,9 @@ founders_dilution = (raise_amount / post_money_valuation) * 100
 series_a_dilution = (series_a_raise_amount / series_a_post_money_valuation) * 100
 
 print(\"Founders dilution: \" + str(founders_dilution) + \"%\")
-        ", user_input)
+
+So generate the Python code that we will execute that can help the user with this question: {}
+        ", user_input, user_input)
     };
 
     // Generate Python code
@@ -88,7 +98,7 @@ print(\"Founders dilution: \" + str(founders_dilution) + \"%\")
                 },
             },
         },
-        user_context: user_input.to_string(),
+        user_context: build_prompt(&user_input),
         model_config: ModelConfig {
             model_name: String::from("open-source/llama-2-70b-chat"),
             model_url: Some("https://api.perplexity.ai/chat/completions".to_string()),
@@ -117,11 +127,15 @@ print(\"Founders dilution: \" + str(founders_dilution) + \"%\")
     // Create Docker container
     let config = Config {
         image: Some("code-interpreter"),
-        cmd: Some(vec!["python", "-c", &python_code]),
         host_config: Some(HostConfig {
             auto_remove: Some(true),
             ..Default::default()
         }),
+        attach_stdin: Some(true),
+        attach_stdout: Some(true),
+        attach_stderr: Some(true),
+        open_stdin: Some(true),
+        tty: Some(true),
         ..Default::default()
     };
     let options = CreateContainerOptions {
@@ -136,37 +150,98 @@ print(\"Founders dilution: \" + str(founders_dilution) + \"%\")
         .start_container(&container.id, None::<StartContainerOptions<String>>)
         .await?;
 
-    println!("Docker container started. Fetching the logs...");
+    // non interactive
+    let exec = docker
+        .create_exec(
+            &container.id,
+            CreateExecOptions {
+                attach_stdout: Some(true),
+                attach_stderr: Some(true),
+                cmd: Some(vec!["python", "-c", &python_code]),
+                ..Default::default()
+            },
+        )
+        .await?
+        .id;
+    let mut exec_stream_result = docker.start_exec(&exec, None);
 
-    // Fetch the logs
-    let mut logs = docker.logs::<String>(
-        &container.id,
-        Some(LogsOptions {
-            stdout: true,
-            stderr: true,
-            ..Default::default()
-        }),
-    );
-    while let Some(log_output) = logs.next().await {
-        match log_output {
-            Ok(output) => println!("{}", output),
-            Err(e) => eprintln!("Error reading logs: {}", e),
+    let mut output = String::new();
+    while let Some(Ok(msg)) = exec_stream_result.next().await {
+        match msg {
+            StartExecResults::Attached { log, .. } => match log {
+                LogOutput::StdOut { message } => {
+                    output.push_str(&String::from_utf8(message.to_vec()).unwrap());
+                }
+                LogOutput::StdErr { message } => {
+                    output.push_str(&String::from_utf8(message.to_vec()).unwrap());
+                }
+                _ => (),
+            },
+            _ => (),
         }
     }
 
-    println!("Logs fetched. Removing the Docker container...");
-
-    // Stop Docker container
+    // remove container
     docker
-        .stop_container(&container.id, None::<StopContainerOptions>)
+        .remove_container(
+            &container.id,
+            Some(RemoveContainerOptions {
+                force: true,
+                ..Default::default()
+            }),
+        )
         .await?;
+    Ok(output)
+}
 
-    // Remove Docker container
-    docker
-        .remove_container(&container.id, None::<RemoveContainerOptions>)
-        .await?;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use dotenv::dotenv;
 
-    println!("Docker container removed.");
+    #[tokio::test]
+    #[ignore]
+    async fn test_interpreter() {
+        dotenv().ok();
 
-    Ok(())
+        let inputs = vec![
+            "Compute the factorial of 10",
+            "Calculate the standard deviation of the numbers 1, 2, 3, 4, 5",
+            "Find the roots of the equation x^2 - 3x + 2 = 0",
+            "Calculate the area under the curve y = x^2 from x = 0 to x = 2",
+            "Compute the integral of x^2 from 0 to 1",
+            "Calculate the determinant of the matrix [[1, 2], [3, 4]]",
+            "Solve the system of equations: 2x + 3y = 7 and x - y = 1",
+            "Compute the eigenvalues of the matrix [[1, 2], [3, 4]]",
+            "Calculate the dot product of the vectors [1, 2, 3] and [4, 5, 6]",
+            "Compute the cross product of the vectors [1, 2, 3] and [4, 5, 6]",
+            "Calculate the Fourier transform of the function f(t) = t^2 for t from -1 to 1",
+            "Compute the inverse of the matrix [[1, 2, 3], [4, 5, 6], [7, 8, 9]]",
+            "Solve the differential equation dy/dx = y^2 with initial condition y(0) = 1",
+            "Calculate the double integral of x*y over the rectangle [0, 1] x [0, 1]",
+            "Compute the Laplace transform of the function f(t) = e^(-t) * sin(t)",
+            "Find the shortest path in the graph with edges {(A, B, 1), (B, C, 2), (A, C, 3)}",
+            "Calculate the convolution of the functions f(t) = t and g(t) = t^2",
+            "Compute the eigenvalues and eigenvectors of the matrix [[1, 2, 3], [4, 5, 6], [7, 8, 9]]",
+            "Solve the system of linear equations: 2x + 3y - z = 1, x - y + 2z = 3, 3x + y - z = 2",
+            "Calculate the triple integral of x*y*z over the cube [0, 1] x [0, 1] x [0, 1]",
+        ];
+
+        for input in inputs {
+            let result = interpreter(input.to_string()).await;
+            assert!(
+                result.is_ok(),
+                "Failed on input: {} error: {:?}",
+                input,
+                result
+            );
+            let result_string = result.unwrap();
+            assert!(
+                result_string.len() > 0,
+                "Failed on input: {}. Result: {}",
+                input,
+                result_string
+            );
+        }
+    }
 }
