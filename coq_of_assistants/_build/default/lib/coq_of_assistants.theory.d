485970dd9a71724c4ddf4c0e933086386f7712c1
mike dupont <mike.dupont@introspector.local>
2024-01-28 06:55:00 -0500

    moving

---
+++ coq_of_assistants/_build/default/lib/coq_of_assistants.theory.d
@@ -0,0 +1,10 @@
+run_consumer.vo run_consumer.glob run_consumer.v.beautified run_consumer.required_vo: run_consumer.v 
+run_consumer.vio: run_consumer.v 
+assistants_extra.vo assistants_extra.glob assistants_extra.v.beautified assistants_extra.required_vo: assistants_extra.v 
+assistants_extra.vio: assistants_extra.v 
+assistants_core.vo assistants_core.glob assistants_core.v.beautified assistants_core.required_vo: assistants_core.v 
+assistants_core.vio: assistants_core.v 
+assistants_benches.vo assistants_benches.glob assistants_benches.v.beautified assistants_benches.required_vo: assistants_benches.v 
+assistants_benches.vio: assistants_benches.v 
+assistants_api_communication.vo assistants_api_communication.glob assistants_api_communication.v.beautified assistants_api_communication.required_vo: assistants_api_communication.v 
+assistants_api_communication.vio: assistants_api_communication.v 
