1beaf1728c0d577c2c9067acc3609b71ab015f96
Louis Beaumont <louis.beaumont@gmail.com>
2024-02-24 11:49:36 +0100

    refactor: converge towards use of config file instead of env var in order to provide more customisation of the underneath logic

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run the executor and server applications concurrently
-hal-9100-executor &
-hal-9100 &
+hal-9100 executor &
+hal-9100 api &
 
 # Wait for all background processes to finish
 wait
